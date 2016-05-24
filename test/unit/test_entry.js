250bc079d944be45d2b1fb3d8b4fe21284dcf66a
Greg Magdsick <greg.magdsick@gmail.com>
2016-05-23 23:04:40 -0700

    working on tests

---
+++ test/unit/test_entry.js
@@ -0,0 +1,5 @@
+require('../../app/js/entry');
+// require('./pedal-controller-test');
+require('./pedal-directive-test');
+// require('./motor-controller-test');
+// require('./motor-directive-test');
