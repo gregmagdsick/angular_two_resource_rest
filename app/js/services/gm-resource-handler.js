1bb834d9907b4a5060fa50b37fe59146afebfbf9
Greg Magdsick <greg.magdsick@gmail.com>
2016-05-31 09:52:28 -0700

    started work

---
+++ app/js/services/gm-resource-handler.js
@@ -0,0 +1,7 @@
+module.exports = function(app) {
+  app.factory('gmResourceHandler', function() { // eslint-disable-line prefer-arrow-callback
+    return function() {
+      
+    };
+  });
+};
--- app/js/services/index.js
+++ app/js/services/index.js
@@ -1,3 +1,4 @@
 module.exports = function(app) {
   require('./gm-handle-error')(app);
+  require('./gm-resource-handler')(app);
 };
