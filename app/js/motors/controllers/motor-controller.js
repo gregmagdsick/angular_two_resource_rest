c13870fe6c58fa0ba2e2bcc4e01d9c516358e3db
Greg Magdsick <greg.magdsick@gmail.com>
2016-05-31 11:35:03 -0700

    motor side working

--- app/js/motors/controllers/motor-controller.js
+++ app/js/motors/controllers/motor-controller.js
@@ -1,20 +1,22 @@
 const baseUrl = require('../../config').baseUrl;
 
 module.exports = function(app) {
-  app.controller('MotorController', ['$http', 'gmHandleError', function($http, gmHandleError) {
-    this.motors = [];
+  app.controller('MotorController', ['$http', 'gmHandleError', 'gmCounter', function($http, gmHandleError, gmCounter) { // eslint-disable-line max-len
+    this.gmCounter = gmCounter;
+    this.motors = this.gmCounter.motorBikes;
     this.getAll = () => {
       $http.get(baseUrl + '/api/motor')
       .then((res) => {
-        this.motors = res.data;
+        this.motors = this.gmCounter.motorBikes = res.data;
       }, gmHandleError(this.errors, 'Could not get motorbikes from server').bind(this));
     };
 
     this.createMotor = () => {
       $http.post(baseUrl + '/api/motor', this.newMotor)
       .then((res) => {
-        this.motors.push(res.data);
         this.newMotor = null;
+        this.motors.push(res.data);
+        this.gmCounter.addMotor(res.data);
       }, gmHandleError(this.errors, 'Could not save new motorbike').bind(this));
     };
 
@@ -29,6 +31,7 @@ module.exports = function(app) {
       $http.delete(baseUrl + '/api/motor/' + motor.model, motor)
       .then(() => {
         this.motors.splice(this.motors.indexOf(motor), 1);
+        this.gmCounter.motorBikes.splice(this.gmCounter.motorBikes.indexOf(motor), 1);
       }, gmHandleError(this.errors, 'Could not delete motorbike: ' + motor.model).bind(this));
     };
 
